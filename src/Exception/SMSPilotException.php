14bd2b618c74ee06d2eb2670dd0de62676ae90ad
Oleg Shtanko <oleg@hotlead.ru>
2019-02-11 23:26:48 +1000

    Mapping error by response code

--- src/Exception/SMSPilotException.php
+++ src/Exception/SMSPilotException.php
@@ -2,7 +2,31 @@
 
 namespace SMSPilot\Exception;
 
+use SMSPilot\SMSPilotErrorEnum;
+
 class SMSPilotException extends \Exception
 {
+    /**
+     * @return integer
+     */
+    public function getErrorCode()
+    {
+        return (int)$this->message;
+    }
 
+    /**
+     * @return string
+     */
+    public function getDescriptionEn()
+    {
+        return SMSPilotErrorEnum::$ERRORS[$this->getErrorCode()]['en'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescriptionRu()
+    {
+        return SMSPilotErrorEnum::$ERRORS[$this->getErrorCode()]['ru'];
+    }
 }
